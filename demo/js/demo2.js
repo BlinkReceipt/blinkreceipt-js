c32834cb818c080932c1de66510a130e981dd53d
GC <gene@xod.ca>
2019-11-24 20:16:56 +0700

    Cross-browser fixes.

--- demo/js/demo2.js
+++ demo/js/demo2.js
@@ -8,22 +8,12 @@ $(document).ready(function() {
     if (!BlinkReceipt.isSecureOrigin()) {
         alert('getUserMedia() must be run from a secure origin: HTTPS or localhost.');
     }
-    if (navigator.mediaDevices && isMobileBrowser()) {
+    if (isMobileBrowser() && navigator.mediaDevices) {
         $('#btnMobileScan').css('display','');
     }
 
     $('#initialChoice').css('top', ($(window).height() - $('#initialChoice').height()) / 2 + 'px');
 });
-
-function isMobileBrowser() {
-    if( navigator.userAgent.match(/Android/i)
-     || navigator.userAgent.match(/iPhone/i)
-     || navigator.userAgent.match(/iPad/i)
-     || navigator.userAgent.match(/iPod/i)) {
-        return true;
-    }
-    return false;
-}
 
 //BlinkReceipt.apiKey = '9b21415cd442f62123b6f94a10942a88';
 //BlinkReceipt.apiKey = '42aa8cae13104d95b5a7972b11c7b6c6';
@@ -36,23 +26,23 @@ BlinkReceipt.cameraCaptureCropPercentBottom = 4;
 BlinkReceipt.cameraCaptureCropPercentLeft = 7;
 BlinkReceipt.cameraCaptureCropPercentRight = 7;
 
-let parentStub = new Object();  // so that we can copy some of the original methods to be called later in our custom callbacks
+var parentStub = new Object();  // so that we can copy some of the original methods to be called later in our custom callbacks
 parentStub['onUserChoseImage'] = BlinkReceipt['onUserChoseImage'];
 
 BlinkReceipt.onCreateUI = function($parentContainer, $elemCenter) {
     BlinkReceipt.showDebugInfo('method', 'onCreateUI');
 
-    let scanFrame = $('<div id="scanFrame" style="display:none;"></div>')
+    var scanFrame = $('<div id="scanFrame" style="display:none;"></div>')
     $parentContainer.append(scanFrame);
 
-    let $elemDivButtonBar = $('<div id="brjs-divButtonBar">');
-    let $elemTableButtons = $('<table border=0 width=100% id="brjs-tblButtons">');
-    let $elemRowButtons = $('<tbody><tr><td align="center"><button id="brjs-btnSecondaryAction" class="brjs-actionButton">Cancel</button></td><td align="center"><button id="brjs-snap" class="brjs-cameraButton"></button></td><td align="center"><button id="brjs-finish" class="brjs-actionButton">Finish</button></td></tr></tbody>');
+    var $elemDivButtonBar = $('<div id="brjs-divButtonBar">');
+    var $elemTableButtons = $('<table border=0 width=100% id="brjs-tblButtons">');
+    var $elemRowButtons = $('<tbody><tr><td align="center"><button id="brjs-btnSecondaryAction" class="brjs-actionButton">Cancel</button></td><td align="center"><button id="brjs-snap" class="brjs-cameraButton"></button></td><td align="center"><button id="brjs-finish" class="brjs-actionButton">Finish</button></td></tr></tbody>');
     $elemTableButtons.append($elemRowButtons);
     $elemDivButtonBar.append($elemTableButtons);
     $elemCenter.append($elemDivButtonBar);  // $elemCenter gets appended to $parentContainer internally after this
 
-    let $elemSpinner = $('<div id="brjs-imgSpinner" style="position: absolute; width: 100px; height: 100px; display: none;">');
+    var $elemSpinner = $('<div id="brjs-imgSpinner" style="position: absolute; width: 100px; height: 100px; display: none;">');
     $elemSpinner.css({
         left: (($(window).width() - $elemSpinner.width()) / 2) + 'px',
         top:  (($(window).height() - $elemSpinner.height()) / 2) + 'px'
@@ -81,11 +71,11 @@ BlinkReceipt.onScanAcquired = function(winningDataUrl) {
     $('#scanFrame').hide();
     $('#brjs-imgSpinner').hide();
 
-    let scaleW = $('#brjs-gum').width() / this.gumVideo.videoWidth;
-    let scaleH = $('#brjs-gum').height() / this.gumVideo.videoHeight;
-    let scaleFactor = Math.min(scaleW, scaleH);
+    var scaleW = $('#brjs-gum').width() / this.gumVideo.videoWidth;
+    var scaleH = $('#brjs-gum').height() / this.gumVideo.videoHeight;
+    var scaleFactor = Math.min(scaleW, scaleH);
 
-    let $imgStatic = $('<img style="position: absolute; display: none">');
+    var $imgStatic = $('<img style="position: absolute; display: none">');
     $imgStatic.css('width', scaleFactor * this.gumVideo.videoWidth + 'px');
     $imgStatic.css('height', scaleFactor * this.gumVideo.videoHeight + 'px');
     $imgStatic.css('left', (screen.width - scaleFactor * this.gumVideo.videoWidth) / 2 + "px");
@@ -111,7 +101,7 @@ BlinkReceipt.onRetakeScan = function() {
     BlinkReceipt.showDebugInfo('method', 'onRetakeScan');
 
     if (BlinkReceipt.staticImages.length > 0) {
-        let lastImg = BlinkReceipt.staticImages.pop();
+        var lastImg = BlinkReceipt.staticImages.pop();
         lastImg.remove();
     }
 
@@ -131,12 +121,12 @@ BlinkReceipt.onAddScanButtonClicked = function() {
 
     //if we have a previous static image, detach and re-insert it below current image so that current image will appear on top
     if (BlinkReceipt.staticImages.length > 1) {
-        let prevImg = BlinkReceipt.staticImages[BlinkReceipt.staticImages.length-2];
+        var prevImg = BlinkReceipt.staticImages[BlinkReceipt.staticImages.length-2];
         prevImg.detach().prependTo($('#brjs-container'));
     }
 
     if (BlinkReceipt.staticImages.length > 0) {
-        let curImg = BlinkReceipt.staticImages[BlinkReceipt.staticImages.length-1];
+        var curImg = BlinkReceipt.staticImages[BlinkReceipt.staticImages.length-1];
         curImg.animate({
             top: "-=" + curImg.height() * 0.95
         });
@@ -217,13 +207,12 @@ BlinkReceipt.apiDomain = 'sandbox.blinkreceipt.com';
 // BlinkReceipt.apiDomain = 'scan-ph.blinkreceipt.com';
 //BlinkReceipt.apiVersion = 9;
 
-var queryParams = new URLSearchParams(window.location.search);
-var slug = queryParams.get('slug');
+var slug = getUrlParameter('slug');
 if (slug && slug.length > 0) {
     BlinkReceipt.validatePromotions = true;
     BlinkReceipt.promotionIds = [slug];
 }
-var apiKey = queryParams.get('api_key');
+var apiKey = getUrlParameter('api_key');
 if (apiKey && apiKey.length > 0) {
     BlinkReceipt.apiKey = apiKey;
 }
@@ -325,8 +314,8 @@ function showProdTable(parseResults) {
     }
 
     if (parseResults.date) {
-        let dateObj = new Date(parseResults.date.value);
-        let purchaseDate = moment(dateObj.toISOString());
+        var dateObj = new Date(parseResults.date.value);
+        var purchaseDate = moment(dateObj.toISOString());
         $('#cellDate').text('Date: ' + purchaseDate.format('MMM D, YYYY'));
     }
     if (parseResults.time) {
@@ -469,4 +458,14 @@ function ucfirst(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function isMobileBrowser() {
+    return (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4)));
+}
 
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
